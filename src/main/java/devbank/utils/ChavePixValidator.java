1dffd254fba8df8f43eda80edc3d5eb215a6beea
italocauu <ivtcau@gmail.com>
2026-06-03 16:53:44 -0300

    update

---
+++ src/main/java/devbank/utils/ChavePixValidator.java
@@ -0,0 +1,17 @@
+packagem devbank.utils;
+
+public class ValidatorEmailUtils{
+
+    private static boolean
+
+    public static boolean isValido(string valor){
+        
+
+
+
+
+
+
+        return true
+    }
+}
